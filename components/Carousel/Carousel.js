7870320445f901c9e1652bfdcd4110c088fe9522
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-17 10:53:00 +0100

    stretch goals: add .less extensions to less files in index

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,37 @@
 class Carousel {
+    constructor(carouselElememt){
+        this.carouselElememt = carouselElememt;
+        this.currentPic = 0;
+        this.imgs = this.carouselElememt.querySelectorAll('img');
+        this.maxPic = this.imgs.length;
 
+        this.carouselElememt.querySelector('.left-button').addEventListener('click', () => this.leftClicked());
+        this.carouselElememt.querySelector('.right-button').addEventListener('click', () => this.rightClicked());
+    }
+
+     rightClicked() {
+        let newNum = this.currentPic + 1;
+        if (newNum == this.maxPic )
+            newNum = 0;
+
+        this.imgs[this.currentPic].classList = 'slideRightOut';
+        this.imgs[newNum].classList = 'slideRightIn';
+        this.currentPic = newNum;
+      }
+
+    leftClicked(){
+        let newNum = this.currentPic - 1;
+        if (newNum == -1 )
+            newNum = this.maxPic - 1;
+        this.imgs[this.currentPic].classList = 'slideLeftOut';
+        this.imgs[newNum].classList = 'slideLeftIn';
+        this.currentPic = newNum;
+
+      }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
